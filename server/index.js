b3b37f598c96bfdbf4cf59b4addd660f30773171
qlfzn <aqilfauzan09@gmail.com>
2025-01-18 14:41:26 +0800

    test commit with zed

--- server/index.js
+++ server/index.js
@@ -9,34 +9,35 @@ const auth = require("./middlewares/auth");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.use("/auth", userRoute);
 app.use("/products", productRoute);
 
 app.get("/", (req, res) => {
-    res.send("This is Pricelisto.")
-})
+  res.send("This is Pricelisto.");
+});
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/free-endpoint", (req, res) => {
-    res.json({
-        message: "Free to access.",
-    })
-})
+  res.json({
+    message: "Free to access.",
+  });
+});
 
 app.get("/auth-endpoint", auth, (req, res) => {
-    res.json({
-        message: "Authorised to access.",
-    })
-})
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is alive at http://localhost:${PORT}`);
-    });
-}).catch((err) => {
-    console.error("Failed to connect to the database", err);
+  res.json({
+    message: "Authorised to access.",
+  });
 });
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is alive at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database", err);
+  });
