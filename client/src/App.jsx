c28e0ff4b68d599b2e8d9e066e7fcca91b1287b2
Aqil Fauzan <124109678+qlfzn@users.noreply.github.com>
2025-02-15 16:54:44 +0800

    refactor to vite project + tailwind (#10)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage/LandingPage';
-import HomePage from './pages/HomePage/HomePage';
-import useAuth from '../hooks/useAuth';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-        <Route path="/login" element={<h2>Login Page (Coming Soon)</h2>} />
-        <Route path="/signup" element={<h2>Sign-Up Page (Coming Soon)</h2>} />
-      </Routes>
-    </Router>
-  );
+const App = () => {
+  return(
+    <div>
+      <h1 class="text-2xl text-black text-bold" >Hello World!</h1>
+    </div>
+  )
 }
 
 export default App;
